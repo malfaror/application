312dfd9fdbd87a3208a9596077dc226e039ea083
malfaror <46729582+malfaror@users.noreply.github.com>
2019-01-20 15:37:40 -0600

    Update prod1.rb

--- chef/cookbooks/google-cloud/recipes/prod1.rb
+++ chef/cookbooks/google-cloud/recipes/prod1.rb
@@ -14,17 +14,11 @@ project 'smiling-breaker-228800'
 credential 'mycred'
 end
 
-gcompute_network 'mynetwork-prod' do
-action :create
-project 'smiling-breaker-228800'
-credential 'mycred'
-end
-
 gcompute_instance 'prod1' do
 action :create
 machine_type 'n1-standard-1'
 disks [ { boot: true, auto_delete: true, source: 'prod1' } ]
-network_interfaces [ { network: 'mynetwork-prod', access_configs: [ { name: 'External NAT', type: 'ONE_TO_ONE_NAT' } ] } ]
+network_interfaces [ { access_configs: [ { name: 'External NAT', type: 'ONE_TO_ONE_NAT' } ] } ]
 zone 'us-east1-b'
 project 'smiling-breaker-228800'
 credential 'mycred'
